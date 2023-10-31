57346a040fcd32769642e60eb417aa765bf212fc
Gea Loro <gea.loro@gmail.com>
2023-10-31 14:32:47 -0700

    Update lab3_2.cpp

--- lab3_2/main/lab3_2.cpp
+++ lab3_2/main/lab3_2.cpp
@@ -6,19 +6,20 @@
 #define SDA_PIN 10
 #define SCL_PIN 8
 
+i2c_config_t conf;
+conf.mode = I2C_MODE_MASTER;
+conf.sda_io_num = SDA_PIN;
+conf.scl_io_num = SCL_PIN;
+conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+i2c_param_config(I2C_NUM_0, &conf);
+i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0);
+    
+DFRobot_LCD lcd(16, 2);
+
 extern "C" {
     void app_main(void)
     {
-        i2c_config_t conf;
-        conf.mode = I2C_MODE_MASTER;
-        conf.sda_io_num = SDA_PIN;
-        conf.scl_io_num = SCL_PIN;
-        conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
-        conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
-        i2c_param_config(I2C_NUM_0, &conf);
-        i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0);
-    
-        DFRobot_LCD lcd(16, 2);
     
         while (1) {
             lcd.init();
