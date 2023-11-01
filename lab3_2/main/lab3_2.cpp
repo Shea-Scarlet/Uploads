24555739520ad11da2836683a77915f396b6f3d0
Gea Loro <gea.loro@gmail.com>
2023-10-31 18:08:16 -0700

    Update lab3_2.cpp

--- lab3_2/main/lab3_2.cpp
+++ lab3_2/main/lab3_2.cpp
@@ -20,10 +20,9 @@ DFRobot_LCD lcd(16, 2);
 extern "C" {
     void app_main(void)
     {
-    
+        lcd.init();
+        lcd.setRGB(0, 255, 0);
         while (1) {
-            lcd.init();
-            lcd.setRGB(0, 255, 0);
             lcd.printstr("Hello CSE121!");
             lcd.cursor(0, 1);
             lcd.printstr("Loro");
